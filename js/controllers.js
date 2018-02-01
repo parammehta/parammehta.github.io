a6cb28012eb0ed292c98474281ee3400de1fb4ae
Param Mehta <param.mehta95@gmail.com>
2018-02-01 00:56:33 -0700

    Logo

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,7 @@
 /** *************Angular controller JS*********************/
 "use strict";
 app.controller('ContactController', function ($scope, $http) {
-    $scope.result = 'hidden';
+    $scope.result = 'hidden'
     $scope.resultMessage;
     $scope.formData; //formData is an object holding the name, email, subject, and message
     $scope.submitButtonDisabled = false;
